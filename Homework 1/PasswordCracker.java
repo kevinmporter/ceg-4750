68ee513d52bc29cdb2d2a9e262bfad00b8d3cd04
Kevin Porter <kevin@kevinporter.me>
2016-02-03 22:46:32 -0500

    Homework 1 complete

--- Homework 1/PasswordCracker.java
+++ Homework 1/PasswordCracker.java
@@ -1,9 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Kevin Porter
+ * CEG-4750 Homework 1
+ * Password cracker
+ * Honestly, I couldn't get this to work with the jar files provided.
+ * My code appears to compile without issue, so the algorithm seems acceptable.
+ * Please see my Python file for something more in-depth (but not functional).
  */
-package passwordcracker;
 import java.io.File;
 
 import de.idyl.winzipaes.AesZipFileDecrypter;
@@ -20,45 +22,53 @@ import java.math.BigInteger;
  * @author lingg
  */
 public class PasswordCracker {
-public static void encrypt(String inputFilename, String zipFilename, String password)
+	public static void encrypt(String inputFilename, String zipFilename, String password)
 			throws Exception {
 
-        AESEncrypter encrypter = new AESEncrypterBC();
-        AESEncrypter encrypter2 = new AESEncrypterBC();
+		AESEncrypter encrypter = new AESEncrypterBC();
+		AESEncrypter encrypter2 = new AESEncrypterBC();
 
-        AesZipFileEncrypter.zipAndEncrypt(
-        		new File(inputFilename), new File(zipFilename), password, encrypter);
+		AesZipFileEncrypter.zipAndEncrypt(
+				new File(inputFilename), new File(zipFilename), password, encrypter);
 	}
 
 	public static void decrypt(String zipFilename, String outputFilename, String password)
 			throws Exception {
 
-        AESDecrypter decrypter = new AESDecrypterBC();
+		AESDecrypter decrypter = new AESDecrypterBC();
 
-        AesZipFileDecrypter dec = new AesZipFileDecrypter(new File(zipFilename), decrypter);
-        ExtZipEntry entry = dec.getEntryList().get(0); // assumes only one item is in the zip file
-        dec.extractEntry(entry, new File(outputFilename), password);
-        dec.close();
+		AesZipFileDecrypter dec = new AesZipFileDecrypter(new File(zipFilename), decrypter);
+		ExtZipEntry entry = dec.getEntryList().get(0); // assumes only one item is in the zip file
+		dec.extractEntry(entry, new File(outputFilename), password);
+		dec.close();
 	}
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception{
-        // TODO code application logic here
-        //encrypt("test.txt", "test.zip", "12345");
-	int i = 4;
-	int j = 12;
-	SecureRandom generator = new SecureRandom();
-	while (i < 12) {
-		while (j < 100000) {
-			String password = new BigInteger(128, generator).toString(i);
-			decrypt("KevinPorter.zip", "KevinPorter.txt", password);
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		//encrypt("test.txt", "test.zip", "12345");
+
+		// this is a super basic brute force algorithm
+		// please see
+		int i = 4;
+		int j = 0;
+		// securely generate characters
+		SecureRandom generator = new SecureRandom();
+		// 4 through 12 character passwords
+		while (i < 12) {
+			while (j < 100000) {
+				// using this method, we get 128-bit secure characters
+				String password = new BigInteger(128, generator).toString(i);
+				try {
+					decrypt("KevinPorter.zip", "KevinPorter.txt", password);
+				} catch (Exception e) {
+
+				}
+				j++;
+			}
+			i++;
 		}
 	}
-    }
 
 }
-
-
-
-
--- Homework 1/password_cracker.py
+++ Homework 1/password_cracker.py
@@ -4,19 +4,32 @@ CEG-4750 Homework 1
 
 Includes a brute-force password-cracking method and a dictionary-based
 password cracking method.
+
+This is very slow, by nature. Also note that I discovered after coding this
+that Python is unable to decrypt AES encrypted zip files, so this actually
+is ineffective, but I wanted to point out the work I did.
+
+At the last minute I added a line that should work given that the user is
+on Windows (I included 7-Zip).
 """
 
 
+from subprocess import call
 from threading import Thread
 from zipfile import ZipFile
 import multiprocessing
 import os
+import platform
 import random
 import string
 
 def unzip(password):
     with ZipFile('KevinPorter.zip') as f:
         try:
+            if 'Windows' in platform.platform() or \
+            'windows' in platform.platform():
+                call('7za.exe e KevinPorter.zip p=' + password,
+                     stdout=open(os.devnull, 'wb'))
             f.extractall(pwd=password)
             print 'Success! The password is: ' + password
             quit()
@@ -27,7 +40,6 @@ def unzip_list(l):
     for i in l:
         for j in xrange(10):
             unzip(i + str(j))
-
 
 threads = multiprocessing.cpu_count()
 
@@ -42,9 +54,9 @@ def brute_force(high):
         unzip(password)
 
 # make use of multithreaded machines to be more efficient
-'''for i in range(0, threads):
+for i in range(0, threads):
     t = Thread(target=brute_force, args=(1000000,))
-    t.start()'''
+    t.start()
 
 def dictionary_attack():
     """
@@ -60,6 +72,7 @@ def dictionary_attack():
         for line in f:
             for word in line.split():
                 dictionary.append(word)
+    # split the dict into equal pieces for each thread
     dict_list = [dictionary[i::threads] for i in xrange(threads) ]
     # again, make use of multithreading
     for i in range(0, threads):
